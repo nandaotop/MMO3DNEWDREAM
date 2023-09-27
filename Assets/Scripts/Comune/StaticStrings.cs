78d42f0edfffb9185f17e90c53be29ec4da4e838
nandaotop <83474890+nandaotop@users.noreply.github.com>
2023-09-27 17:43:45 -0300

    camera

---
+++ Assets/Scripts/Comune/StaticStrings.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticStrings
+{
+    public static string vertical = "Vertical";
+    public static string Scrool = "Mouse ScrollWheel";
+    public static string mouseX = "Mouse X";
+    public static string horizontal = "Horizontal";
+    public static string player = "Player";
+    public static string enemy = "Enemy";
+    public static string follow = "CameraFollow";
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Player : Entity
 {
+    CameraFollow follow;
+    [SerializeField] float rotSpeed = 2;
+    float scroolAmount = 3;
+    [SerializeField] float minZoon = 10, maxZoom = 120;
     public override void Init()
     {
         base.Init();
+        if (!photonView.IsMine) return;
+
+        var f = Resources.Load<CameraFollow>(StaticStrings.follow);
+        follow = Instantiate(f, transform.position, transform.rotation);
+        follow.Init(transform);
     }
 
     public override void Tick()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
+        float x = Input.GetAxisRaw(StaticStrings.horizontal);
+        float y = Input.GetAxisRaw(StaticStrings.vertical);
         Vector3 moveInput = new Vector3(x, 0f, y).normalized;
 
         if (moveInput != Vector3.zero)
@@ -30,7 +39,21 @@ public class Player : Entity
         }
 
         sync.Move(x, y);
+        UseCamera();
     }
 
-
+    void UseCamera()
+    {
+        float x = Input.GetAxisRaw(StaticStrings.mouseX);
+        float scrool = Input.GetAxisRaw(StaticStrings.Scrool);
+        Vector3 rot = follow.transform.rotation.eulerAngles;
+        follow.transform.rotation = Quaternion.Euler(rot.x, rot.y + x * rotSpeed, rot.z);
+        if (scrool != 0)
+        {
+            float val = scroolAmount * scrool;
+            val += follow.cam.fieldOfView;
+            val = Mathf.Clamp(val, minZoon, maxZoom);
+            follow.cam.fieldOfView = val;
+        }
+    }
 }
